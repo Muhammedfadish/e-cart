ef12de7fc71cb5ca7a54933a7c47486900784eff
Muhammedfadish <fadishfadi8@gmail.com>
2024-11-20 10:05:19 +0530

    e-cart

---
+++ src/redux/cartStore.js
@@ -0,0 +1,14 @@
+import { configureStore, createReducer, createSlice } from "@reduxjs/toolkit";
+import productSlice from './slices/productSlice'
+import wishlistSlice from './slices/wishListSlice'
+import cartSlice from './slices/cartSlice'
+
+const cartStore = configureStore({
+    reducer:{
+        productReducer : productSlice,
+        wishlistReducer : wishlistSlice,
+        cartReducer:cartSlice 
+    }
+
+})
+export default cartStore
---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name:'cartSlice',
+    initialState:[],
+    reducers:{
+        //action-name : reducer function
+        addToCart:(state,actionByComponent)=>{
+            const existingProduct = state.find(item=>item.id==actionByComponent.payload.id)
+            if(existingProduct){
+                existingProduct.quantity++
+                existingProduct.totalPrice = existingProduct.quantity * existingProduct.price
+                const remainingProducts = state.filter(item=>item.id!=existingProduct.id)
+                state = [...remainingProducts,existingProduct]
+            }else{
+                state.push({...actionByComponent.payload,quantity:1,totalPrice:actionByComponent.payload.price})
+            }
+        },
+        //+ in cart section
+        incrementQuantity : (state,actionByCart)=>{
+            const existingProduct = state.find(item=>item.id==actionByCart.payload)
+            existingProduct.quantity++
+            existingProduct.totalPrice = existingProduct.quantity * existingProduct.price
+            const remainingProducts = state.filter(item=>item.id!=existingProduct.id)
+            state = [...remainingProducts,existingProduct]
+        },
+        //- in cart section
+        decrementQuantity : (state,actionByCart)=>{
+           
+            const existingProduct = state.find(item=>item.id==actionByCart.payload)
+            existingProduct.quantity--
+            existingProduct.totalPrice = existingProduct.quantity * existingProduct.price
+            
+            const remainingProducts = state.filter(item=>item.id!=existingProduct.id)
+            state = [...remainingProducts,existingProduct]
+        },
+        // delete in cart section
+        removeCartItem : (state,actionByCart)=>{
+            return state.filter(item=>item.id!=actionByCart.payload)
+
+        },
+        //empty cart
+        emptyCart : (state)=>{
+            return state = []
+        }
+
+    }
+})
+
+export const {addToCart,incrementQuantity,removeCartItem,decrementQuantity,emptyCart} = cartSlice.actions
+export default cartSlice.reducer
